d40c476fe75c61ddf67e0ffebe1ce411ae63188a
vipulnitt <vp98504@gmail.com>
2023-03-17 23:15:58 +0530

    first commit

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,9 @@
 const StudentsData = [
   {
     id: 1,
-    firstName: 'Susan',
-    lastName: 'Jordon',
-    email: 'susan@example.com',
+    firstName: 'Vipul',
+    lastName: 'Patel',
+    email: 'vipul@example.com',
     startTime: '',
     endTime: '',
     date: '2019-04-11',
